66e23e2f834781a7c5b2a376be29055ed5be01c7
Américo Jerez <jerez.americo@gmail.com>
2025-02-02 13:30:12 -0300

    finish projectile

--- player/basic_range_projectile.gd
+++ player/basic_range_projectile.gd
@@ -2,9 +2,12 @@ extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var duration_timer: Timer = $DurationTimer
+@onready var impact_timer: Timer = $ImpactTimer
 
 var speed := 500.0
 var duration_time := 2.0
+var has_impacted = false
+var impact_time := 0.2
 
 var direction: float
 var spawn_position: Vector2
@@ -17,12 +20,22 @@ func _ready() -> void:
 	duration_timer.one_shot = true
 	duration_timer.wait_time = duration_time
 	duration_timer.start()
+	impact_timer.one_shot = true
+	impact_timer.wait_time = impact_time
+	velocity = Vector2(speed,0).rotated(direction)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	velocity = Vector2(speed,0).rotated(direction)
-	animation_player.play("shooting")
-	move_and_slide()
-	if duration_timer.is_stopped():
+	if has_impacted and impact_timer.is_stopped():
 		self.queue_free()
+	elif !has_impacted and (duration_timer.is_stopped() or is_on_ceiling() or
+			is_on_floor() or is_on_wall()):
+		print("IMPACTED!")
+		has_impacted = true
+		impact_timer.start()
+		animation_player.play("impacting")
+		velocity = Vector2.ZERO
+	elif !has_impacted:
+		animation_player.play("shooting")
+		move_and_slide()
